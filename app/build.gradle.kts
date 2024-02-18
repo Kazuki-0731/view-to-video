ed8cb3d1a4606da4c1955d456e84d4d723438fa4
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2024-02-19 00:17:35 +0900

    update minSdk, compileSdk. rename files com.example -> ishino.kzk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ishino.kzk.view_to_video"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ishino.kzk.view_to_video"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
