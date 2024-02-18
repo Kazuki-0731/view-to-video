cc09d59dcdf66e0df691011f066f2c37f1f33566
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2024-02-19 01:17:26 +0900

    Application作成

---
+++ app/src/main/java/ishino/kzk/view_to_video/App.kt
@@ -0,0 +1,11 @@
+package ishino.kzk.view_to_video
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
