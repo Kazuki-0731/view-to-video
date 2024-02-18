8cd7157b13953b1bc5eaa311c50d44e48a81c867
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2024-02-18 23:54:11 +0900

    first commit

---
+++ app/src/main/java/com/example/view_to_video/MainActivity.kt
@@ -0,0 +1,11 @@
+package ishino.kzk.view_to_video
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
